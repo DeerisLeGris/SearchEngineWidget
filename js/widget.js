b98cb9b1ac17e49b511b5f2202d8404f4a47aeb4
DeerisLeGris <yoshi_gris@outlook.com>
2019-04-03 16:58:35 +0200

    event updated

--- js/widget.js
+++ js/widget.js
@@ -95,7 +95,7 @@ class SearchEngineController extends WidgetController {
 			text.innerText = jsonParsed[1][i];
 			div.appendChild(text);
 
-			div.addEventListener("click", event => { openTabsResults(jsonParsed[1][i]) });
+			div.addEventListener("click", event => { this.try.mvc.controller.openTabsResults(jsonParsed[1][i]) });
 			results.push(div);
 		}
 
