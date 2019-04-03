5cf277266772e4f36c3cb5c1cb601f3f8724a732
DeerisLeGris <yoshi_gris@outlook.com>
2019-04-03 17:02:53 +0200

    widget fixed

--- js/widget.js
+++ js/widget.js
@@ -95,8 +95,7 @@ class SearchEngineController extends WidgetController {
 			text.innerText = jsonParsed[1][i];
 			div.appendChild(text);
 
-			//div.addEventListener("click", event => { this.try.mvc.controller.openTabsResults(jsonParsed[1][i]) });
-			Events.on(this.try.div, "click", event => this.try.mvc.controller.openTabsResults(jsonParsed[1][i]));
+			div.addEventListener("click", event => { this.try.openTabsResults(jsonParsed[1][i]) });
 			results.push(div);
 		}
 
